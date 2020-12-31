3915282c0da017684624ae48611e1d0832496e69
mpd <mpd@jesters-court.net>
2020-12-30 23:38:50 -0800

    handle constant 1 in int_node to_asm

--- src/ast/int_node.rs
+++ src/ast/int_node.rs
@@ -3,6 +3,8 @@ use crate::asm::instruction::Instruction;
 use crate::asm::register_counter::RegisterCounter;
 use crate::asm::inst::iconst0::IConst0;
 use crate::asm::inst::iconst::IConst;
+use crate::asm::register::Register;
+use crate::asm::inst::iconst1::IConst1;
 
 #[derive(Debug, Copy, Clone)]
 pub struct IntNode {
@@ -17,6 +19,7 @@ impl ASTNode for IntNode {
     fn to_asm(&self) -> Vec<Box<Instruction>> {
         match self.value {
             0 => vec![Box::new(IConst0(RegisterCounter::next()).into())],
+            1 => vec![Box::new(IConst1(RegisterCounter::next()).into())],
             n => vec![Box::new(IConst(RegisterCounter::next(), n).into())]
         }
     }
