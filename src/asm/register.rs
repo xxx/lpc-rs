8503b144e44f5f0241b127216efb4d6250aa1cfc
mpd <mpd@jesters-court.net>
2021-01-09 13:50:47 -0800

    add a value method to register for readability

--- src/asm/register.rs
+++ src/asm/register.rs
@@ -3,6 +3,12 @@ use std::fmt::{Display, Formatter};
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Default)]
 pub struct Register(pub usize);
 
+impl Register {
+    pub fn value(&self) -> usize {
+        self.0
+    }
+}
+
 impl Display for Register {
     fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
         write!(f, "r{}", self.0)
@@ -12,6 +18,12 @@ impl Display for Register {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_value_is_correct() {
+        let register = Register(666);
+        assert_eq!(register.value(), register.0);
+    }
 
     #[test]
     fn test_display_displays_with_the_prefix() {
