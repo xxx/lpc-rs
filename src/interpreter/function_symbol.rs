5a866e8181253d8abe167b3f48184c0977a7c4e8
mpd <mpd@jesters-court.net>
2021-01-27 01:11:00 -0800

    doc tweaks

--- src/interpreter/function_symbol.rs
+++ src/interpreter/function_symbol.rs
@@ -4,6 +4,7 @@
 pub struct FunctionSymbol {
     pub name: String,
     pub num_args: usize,
+    /// The number of non-argument, non-return-value locals. Used for register allocation.
     pub num_locals: usize,
     /// The address of this function in memory.
     pub address: usize
