8a64081b7be911c376bffef3bc4b90740027105e
mpd <mpd@jesters-court.net>
2021-02-07 13:59:06 -0800

    update print to print out the actual contents of an array, recursively if necessary

--- src/interpreter/efun/print.rs
+++ src/interpreter/efun/print.rs
@@ -3,8 +3,28 @@ use crate::interpreter::lpc_var::LPCVar;
 use crate::interpreter::asm_interpreter::AsmInterpreter;
 use crate::interpreter::lpc_value::LPCValue;
 
+fn print_array(arr: &Vec<LPCVar>, interpreter: &AsmInterpreter, indent: usize) {
+    println!("{:width$}({{", "", width = indent);
+
+    for var in arr {
+        let val = interpreter.resolve_var(var);
+        print_val(val, interpreter, indent + 2);
+    }
+
+    println!("{:width$}}})", "", width = indent);
+}
+
+fn print_val(val: LPCValue, interpreter: &AsmInterpreter, indent: usize) {
+    if let LPCValue::Array(a) = &val {
+        print_array(a, interpreter, 0);
+    } else {
+        println!("{:width$}{}", "", val, width = indent);
+    }
+}
+
 pub fn print(frame: &StackFrame, interpreter: &AsmInterpreter) {
-    // function arguments start in register 1.
+    // function arguments start in register 1, and we know this function has only 1 arg.
     let val = interpreter.resolve_register(1);
-    println!("{}", val);
+
+    print_val(val, interpreter, 0);
 }
