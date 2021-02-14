f16b646ceaf441fa68e443ea6acf6e77adba51bf
mpd <mpd@jesters-court.net>
2021-02-13 16:40:48 -0800

    cargo fix

--- src/bin/lpcc.rs
+++ src/bin/lpcc.rs
@@ -4,8 +4,7 @@ use lpc_rs::{
     ast::ast_node::ASTNodeTrait,
     codegen::{
         asm_tree_walker::AsmTreeWalker, default_params_walker::DefaultParamsWalker,
-        scope_walker::ScopeWalker, semantic_check_walker::SemanticCheckWalker,
-        tree_printer::TreePrinter, tree_walker::TreeWalker,
+        scope_walker::ScopeWalker, semantic_check_walker::SemanticCheckWalker, tree_walker::TreeWalker,
     },
     errors,
     errors::compiler_error::{parse_error::ParseError, CompilerError},
