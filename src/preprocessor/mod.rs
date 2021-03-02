ab381915796109688d96bbe58c2d21b344a79c18
mpd <mpd@jesters-court.net>
2021-03-01 18:45:42 -0800

    increase robustness in file inclusion, and emit the in-game absolute path to a file in #line directives

--- src/preprocessor/mod.rs
+++ src/preprocessor/mod.rs
@@ -33,6 +33,10 @@ pub struct Preprocessor {
     skip_lines: bool,
 }
 
+lazy_static! {
+    static ref DOUBLE_SLASHES: Regex = Regex::new(r"//").unwrap();
+}
+
 impl Preprocessor {
     /// Create a new `Preprocessor`
     ///
@@ -75,23 +79,47 @@ impl Preprocessor {
     /// # Arguments
     /// `path` - An in-game path.
     /// `cwd` - The current working directory, needed to resolve relative paths.
-    fn canonicalize_path(&self, path: &str, cwd: &str) -> Result<PathBuf, PreprocessorError> {
+    fn canonicalize_path<T, U>(&self, path: T, cwd: U) -> Result<PathBuf, PreprocessorError>
+    where
+        T: AsRef<Path>,
+        U: AsRef<Path>
+    {
+        let path_ref = path.as_ref().to_str().unwrap();
         let sep = String::from(std::path::MAIN_SEPARATOR);
+        let root_string = String::from(self.root_dir.to_str().unwrap());
         // Do this the hard way because .join/.push overwrite if the arg starts with "/"
-        let localized_path = if path.starts_with(&sep) {
-            String::from(self.root_dir.to_str().unwrap()) + &sep + path
+        let localized_path = if path_ref.starts_with(&sep) {
+            root_string + &sep + path_ref
         } else {
-            String::from(self.root_dir.to_str().unwrap()) + &sep + cwd + &sep + path
+            root_string + &sep + cwd.as_ref().to_str().unwrap() + &sep + path_ref
         };
 
-        Ok(Path::new(&localized_path).absolutize().unwrap().to_path_buf())
+        Ok(Path::new(&*DOUBLE_SLASHES.replace_all(&localized_path, "/")).absolutize().unwrap().to_path_buf())
     }
 
-    /// Convert an in-game path, relative or absolute, to a canonical on-server path.
-    /// This function is used for resolving included files.
+    /// Convert an in-game path, relative or absolute, to a canonical in-game path.
     ///
     /// # Arguments
-    /// `path` - An in-game path to the file being scanned.
+    /// `path` - An in-game path.
+    /// `cwd` - The current working directory, needed to resolve relative paths.
+    fn canonicalize_local_path<T, U>(&self, path: T, cwd: U) -> Result<PathBuf, PreprocessorError>
+    where
+        T: AsRef<Path>,
+        U: AsRef<Path>
+    {
+        println!("canon local {}, {}", path.as_ref().display(), cwd.as_ref().display());
+        let canon = self.canonicalize_path(path, cwd).unwrap();
+        let buf = canon.to_str().unwrap();
+        let root_str = self.root_dir.to_str().unwrap();
+
+        Ok(PathBuf::from(&*DOUBLE_SLASHES.replace_all(&buf.chars().skip(root_str.len()).collect::<String>(), "/")))
+    }
+
+    /// Scan a file's contents, transforming as necessary according to the preprocessing rules.
+    ///
+    /// # Arguments
+    /// `filename` - The name of the file being scanned.
+    /// `cwd` - The current working directory on-server, used to resolve relative links
     /// `file_content` - The actual content of the file to scan.
     ///
     /// # Examples
@@ -100,18 +128,19 @@ impl Preprocessor {
     /// let mut preprocessor = Preprocessor::new(".", vec!["/include", "/sys/include"]);
     ///
     /// let content = r#"
-    ///     #include "simple.h"
+    ///     #include "include/simple.h"
     ///
     ///     void main() {
     ///         int a = 123;
     ///     }
     /// "#;
     ///
-    /// let processed = preprocessor.scan("my/file/path", content);
+    /// let processed = preprocessor.scan("file.c", "/", content);
     /// ```
-    pub fn scan<T>(&mut self, path: T, file_content: &str) -> Result<String, PreprocessorError>
+    pub fn scan<T, U>(&mut self, filename: T, cwd: U, file_content: &str) -> Result<String, PreprocessorError>
     where
         T: AsRef<Path>,
+        U: AsRef<Path>,
     {
         lazy_static! {
             static ref SYS_INCLUDE: Regex =
@@ -124,8 +153,11 @@ impl Preprocessor {
 
         let mut output = String::new();
 
+        let p = filename.as_ref().file_name().unwrap().to_str().unwrap();
+        let canonical_path = self.canonicalize_local_path(p, &cwd).unwrap();
+
         let format_line = |current| {
-            format!("#line {} \"{}\"\n", current, path.as_ref().display())
+            format!("#line {} \"{}\"\n", current, canonical_path.display())
         };
 
         output.push_str(&format_line(current_line));
@@ -139,8 +171,7 @@ impl Preprocessor {
                 ));
             } else if let Some(captures) = LOCAL_INCLUDE.captures(line) {
                 let matched = captures.get(1).unwrap();
-                let cwd = path.as_ref().parent().unwrap().to_str().unwrap();
-                let included = self.include_local_file(matched.as_str(), cwd)?;
+                let included = self.include_local_file(matched.as_str(), &cwd)?;
                 output.push_str(&included);
                 if !output.ends_with("\n") {
                     output.push_str("\n");
@@ -156,15 +187,18 @@ impl Preprocessor {
         Ok(output)
     }
 
-    fn include_local_file(&mut self, path: &str, cwd: &str) -> Result<String, PreprocessorError> {
-        let canon_include_path = self.canonicalize_path(path, cwd)?;
-
+    fn include_local_file<T, U>(&mut self, path: T, cwd: U) -> Result<String, PreprocessorError>
+    where
+        T: AsRef<Path>,
+        U: AsRef<Path>
+    {
+        let canon_include_path = self.canonicalize_path(&path, &cwd)?;
         let true_root = PathBuf::from(&self.root_dir);
 
         if !canon_include_path.starts_with(true_root) {
             return Err(PreprocessorError(format!(
                 "Attempt to include a file outside the root: `{}` (expanded to `{}`)",
-                path,
+                path.as_ref().display(),
                 canon_include_path.display()
             )));
         }
@@ -174,13 +208,17 @@ impl Preprocessor {
             Err(e) => {
                 return Err(PreprocessorError(format!(
                     "Error including file `{}`: {:?}",
-                    path,
+                    path.as_ref().display(),
                     e
                 )))
             }
         };
 
-        self.scan(path, &file_content)
+        let local_canon_include_path = self.canonicalize_local_path(&path, &cwd)?;
+        let filename = local_canon_include_path.file_name().unwrap().to_str().unwrap();
+        let cwd = local_canon_include_path.parent().unwrap().to_str().unwrap();
+        println!("about to scan {} {}", filename, cwd);
+        self.scan(filename, cwd, &file_content)
     }
 }
 
@@ -211,12 +249,12 @@ mod tests {
 
         fn test_include(input: &str, expected: &str) {
             let mut preprocessor = fixture();
-            match preprocessor.scan("test.c", input) {
+            match preprocessor.scan("test.c", "/", input) {
                 Ok(result) => {
                     assert_eq!(result, expected)
                 }
                 Err(e) => {
-                    panic!(e)
+                    panic!(format!("{:?}", e))
                 }
             }
         }
@@ -224,7 +262,7 @@ mod tests {
         // `expected` is converted to a Regex, for easier matching on errors.
         fn test_include_error(input: &str, expected: &str) {
             let mut preprocessor = fixture();
-            match preprocessor.scan("test.c", input) {
+            match preprocessor.scan("test.c", "/", input) {
                 Ok(result) => {
                     panic!("Expected to fail, but passed with {}", result);
                 }
@@ -240,10 +278,10 @@ mod tests {
             let input = r#"#include "include/simple.h""#;
 
             let expected = indoc! {r#"
-                #line 1 "test.c"
-                #line 1 "include/simple.h"
+                #line 1 "/test.c"
+                #line 1 "/include/simple.h"
                 1 + 2 + 3 + 4 + 5;
-                #line 2 "test.c"
+                #line 2 "/test.c"
             "#};
 
             test_include(input, expected);
@@ -254,12 +292,12 @@ mod tests {
             let input = r#"#include "include/level_2/two_level.h""#;
 
             let expected = indoc! {r#"
-                #line 1 "test.c"
-                #line 1 "include/level_2/two_level.h"
-                #line 1 "../simple.h"
+                #line 1 "/test.c"
+                #line 1 "/include/level_2/two_level.h"
+                #line 1 "/include/simple.h"
                 1 + 2 + 3 + 4 + 5;
-                #line 2 "include/level_2/two_level.h"
-                #line 2 "test.c"
+                #line 2 "/include/level_2/two_level.h"
+                #line 2 "/test.c"
             "#};
 
             test_include(input, expected);
@@ -274,16 +312,16 @@ mod tests {
             "#};
 
             let expected = indoc! {r#"
-                #line 1 "test.c"
-                #line 1 "include/level_2/two_level.h"
-                #line 1 "../simple.h"
+                #line 1 "/test.c"
+                #line 1 "/include/level_2/two_level.h"
+                #line 1 "/include/simple.h"
                 1 + 2 + 3 + 4 + 5;
-                #line 2 "include/level_2/two_level.h"
-                #line 2 "test.c"
+                #line 2 "/include/level_2/two_level.h"
+                #line 2 "/test.c"
                 int j = 123;
-                #line 1 "include/simple.h"
+                #line 1 "/include/simple.h"
                 1 + 2 + 3 + 4 + 5;
-                #line 4 "test.c"
+                #line 4 "/test.c"
             "#};
 
             test_include(input, expected);
@@ -294,10 +332,10 @@ mod tests {
             let input = r#"#include "/include/simple.h""#;
 
             let expected = indoc! {r#"
-                #line 1 "test.c"
+                #line 1 "/test.c"
                 #line 1 "/include/simple.h"
                 1 + 2 + 3 + 4 + 5;
-                #line 2 "test.c"
+                #line 2 "/test.c"
             "#};
 
             test_include(input, expected);
