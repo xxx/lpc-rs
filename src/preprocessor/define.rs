55056c919e00e1bf084ff5b7a54052388fdc49ce
mpd <mpd@jesters-court.net>
2021-04-29 20:51:59 -0700

    add some documentation for Define

--- src/preprocessor/define.rs
+++ src/preprocessor/define.rs
@@ -3,18 +3,25 @@ use crate::{
     preprocessor::preprocessor_node::PreprocessorNode,
 };
 
+/// An object macro, e.g. `#define FOO 123`
 #[derive(Debug)]
 pub struct ObjectMacro {
+    /// The vector of Tokens being defined
     pub tokens: Vec<Spanned<Token>>,
+    /// The preprocessor-parsed expression, for evaluating `#if` directives
     pub expr: PreprocessorNode,
 }
 
+/// A function macro, e.g. `#define FOO(a, b) (a * b)`
 #[derive(Debug)]
 pub struct FunctionMacro {
+    /// The vector of Tokens being defined
     pub tokens: Vec<Spanned<Token>>,
+    /// The vector of defined arguments, which are matched against during expansion for replacement
     pub args: Vec<String>,
 }
 
+/// Enumeration of `#define` types
 #[derive(Debug)]
 pub enum Define {
     Object(ObjectMacro),
@@ -22,19 +29,13 @@ pub enum Define {
 }
 
 impl Define {
+    /// Create a new object macro
     pub fn new_object(tokens: Vec<Spanned<Token>>, expr: PreprocessorNode) -> Self {
         Define::Object(ObjectMacro { tokens, expr })
     }
 
+    /// Create a new function macro
     pub fn new_function(tokens: Vec<Spanned<Token>>, args: Vec<String>) -> Self {
         Define::Function(FunctionMacro { tokens, args })
-    }
-
-    pub fn is_object(&self) -> bool {
-        matches!(self, Define::Object(_))
-    }
-
-    pub fn is_function(&self) -> bool {
-        matches!(self, Define::Function(_))
     }
 }
