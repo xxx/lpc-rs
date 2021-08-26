a461e0434484255f1a9166cdb99c47473f63c33d
mpd <mpd@jesters-court.net>
2021-08-25 19:46:06 -0700

    alphabetize TreeWalker methods

--- src/codegen/tree_walker.rs
+++ src/codegen/tree_walker.rs
@@ -24,14 +24,36 @@ pub trait ContextHolder {
 
 /// A trait for types that can walk abstract syntax trees
 pub trait TreeWalker {
-    /// Visit a program node. This is the top-level translation unit.
-    fn visit_program(&mut self, node: &mut ProgramNode) -> Result<()>
+    /// Visit an array literal node
+    fn visit_array(&mut self, node: &mut ArrayNode) -> Result<()>
     where
         Self: Sized,
     {
-        for expr in &mut node.body {
-            expr.visit(self)?;
+        for node in &mut node.value {
+            node.visit(self)?;
         }
+
+        Ok(())
+    }
+
+    /// Visit an assignment node
+    fn visit_assignment(&mut self, node: &mut AssignmentNode) -> Result<()>
+    where
+        Self: Sized,
+    {
+        node.lhs.visit(self)?;
+        node.rhs.visit(self)?;
+
+        Ok(())
+    }
+
+    /// Visit a binary operation node
+    fn visit_binary_op(&mut self, node: &mut BinaryOpNode) -> Result<()>
+    where
+        Self: Sized,
+    {
+        node.l.visit(self)?;
+        node.r.visit(self)?;
 
         Ok(())
     }
@@ -60,65 +82,13 @@ pub trait TreeWalker {
         Ok(())
     }
 
-    /// Visit an int (literal) node
-    fn visit_int(&mut self, _node: &mut IntNode) -> Result<()> {
-        Ok(())
-    }
-
-    /// Visit a float (literal) node
-    fn visit_float(&mut self, _node: &mut FloatNode) -> Result<()> {
-        Ok(())
-    }
-
-    /// Visit a string (literal) node
-    fn visit_string(&mut self, _node: &mut StringNode) -> Result<()> {
-        Ok(())
-    }
-
-    /// Visit a binary operation node
-    fn visit_binary_op(&mut self, node: &mut BinaryOpNode) -> Result<()>
+    /// Visit a comma expression
+    fn visit_comma_expression(&mut self, node: &mut CommaExpressionNode) -> Result<()>
     where
         Self: Sized,
     {
-        node.l.visit(self)?;
-        node.r.visit(self)?;
-
-        Ok(())
-    }
-
-    /// Visit a binary operation node
-    fn visit_unary_op(&mut self, node: &mut UnaryOpNode) -> Result<()>
-    where
-        Self: Sized,
-    {
-        node.expr.visit(self)?;
-
-        Ok(())
-    }
-
-    /// Visit a function definition node
-    fn visit_function_def(&mut self, node: &mut FunctionDefNode) -> Result<()>
-    where
-        Self: Sized,
-    {
-        for parameter in &mut node.parameters {
-            parameter.visit(self)?;
-        }
-
-        for expression in &mut node.body {
-            expression.visit(self)?;
-        }
-
-        Ok(())
-    }
-
-    /// Visit a function return node
-    fn visit_return(&mut self, node: &mut ReturnNode) -> Result<()>
-    where
-        Self: Sized,
-    {
-        if let Some(expression) = &mut node.value {
-            expression.visit(self)?;
+        for expr in &mut node.value {
+            let _ = expr.visit(self);
         }
 
         Ok(())
@@ -136,115 +106,6 @@ pub trait TreeWalker {
         Ok(())
     }
 
-    /// Visit a variable initialization node
-    fn visit_var_init(&mut self, node: &mut VarInitNode) -> Result<()>
-    where
-        Self: Sized,
-    {
-        if let Some(expr) = &mut node.value {
-            expr.visit(self)?;
-        }
-
-        Ok(())
-    }
-
-    /// Visit a variable use node
-    fn visit_var(&mut self, _node: &mut VarNode) -> Result<()>
-    where
-        Self: Sized,
-    {
-        Ok(())
-    }
-
-    /// Visit an assignment node
-    fn visit_assignment(&mut self, node: &mut AssignmentNode) -> Result<()>
-    where
-        Self: Sized,
-    {
-        node.lhs.visit(self)?;
-        node.rhs.visit(self)?;
-
-        Ok(())
-    }
-
-    /// Visit an array literal node
-    fn visit_array(&mut self, node: &mut ArrayNode) -> Result<()>
-    where
-        Self: Sized,
-    {
-        for node in &mut node.value {
-            node.visit(self)?;
-        }
-
-        Ok(())
-    }
-
-    /// Visit a mapping literal node
-    fn visit_mapping(&mut self, node: &mut MappingNode) -> Result<()>
-    where
-        Self: Sized,
-    {
-        for (key, value) in &mut node.value {
-            key.visit(self)?;
-            value.visit(self)?;
-        }
-
-        Ok(())
-    }
-
-    /// Visit a range literal
-    fn visit_range(&mut self, node: &mut RangeNode) -> Result<()>
-    where
-        Self: Sized,
-    {
-        if let Some(expr) = &mut *node.l {
-            expr.visit(self)?;
-        }
-
-        if let Some(expr) = &mut *node.r {
-            expr.visit(self)?;
-        }
-
-        Ok(())
-    }
-
-    /// Visit a comma expression
-    fn visit_comma_expression(&mut self, node: &mut CommaExpressionNode) -> Result<()>
-    where
-        Self: Sized,
-    {
-        for expr in &mut node.value {
-            let _ = expr.visit(self);
-        }
-
-        Ok(())
-    }
-
-    /// Visit an `if` statement
-    fn visit_if(&mut self, node: &mut IfNode) -> Result<()>
-    where
-        Self: Sized,
-    {
-        let _ = node.condition.visit(self);
-        let _ = node.body.visit(self);
-        if let Some(n) = &mut *node.else_clause {
-            let _ = n.visit(self);
-        }
-
-        Ok(())
-    }
-
-    /// Visit a `while` loop
-    fn visit_while(&mut self, node: &mut WhileNode) -> Result<()>
-    where
-        Self: Sized,
-    {
-        let _ = node.condition.visit(self);
-        let _ = node.body.visit(self);
-
-        Ok(())
-    }
-
     /// Visit a `do {} while` loop
     fn visit_do_while(&mut self, node: &mut DoWhileNode) -> Result<()>
     where
@@ -253,6 +114,11 @@ pub trait TreeWalker {
         let _ = node.body.visit(self);
         let _ = node.condition.visit(self);
 
+        Ok(())
+    }
+
+    /// Visit a float (literal) node
+    fn visit_float(&mut self, _node: &mut FloatNode) -> Result<()> {
         Ok(())
     }
 
@@ -277,6 +143,99 @@ pub trait TreeWalker {
         Ok(())
     }
 
+    /// Visit a function definition node
+    fn visit_function_def(&mut self, node: &mut FunctionDefNode) -> Result<()>
+    where
+        Self: Sized,
+    {
+        for parameter in &mut node.parameters {
+            parameter.visit(self)?;
+        }
+
+        for expression in &mut node.body {
+            expression.visit(self)?;
+        }
+
+        Ok(())
+    }
+
+    /// Visit an `if` statement
+    fn visit_if(&mut self, node: &mut IfNode) -> Result<()>
+    where
+        Self: Sized,
+    {
+        let _ = node.condition.visit(self);
+        let _ = node.body.visit(self);
+        if let Some(n) = &mut *node.else_clause {
+            let _ = n.visit(self);
+        }
+
+        Ok(())
+    }
+
+    /// Visit an int (literal) node
+    fn visit_int(&mut self, _node: &mut IntNode) -> Result<()> {
+        Ok(())
+    }
+
+    /// Visit a mapping literal node
+    fn visit_mapping(&mut self, node: &mut MappingNode) -> Result<()>
+    where
+        Self: Sized,
+    {
+        for (key, value) in &mut node.value {
+            key.visit(self)?;
+            value.visit(self)?;
+        }
+
+        Ok(())
+    }
+
+    /// Visit a program node. This is the top-level translation unit.
+    fn visit_program(&mut self, node: &mut ProgramNode) -> Result<()>
+    where
+        Self: Sized,
+    {
+        for expr in &mut node.body {
+            expr.visit(self)?;
+        }
+
+        Ok(())
+    }
+
+    /// Visit a range literal
+    fn visit_range(&mut self, node: &mut RangeNode) -> Result<()>
+    where
+        Self: Sized,
+    {
+        if let Some(expr) = &mut *node.l {
+            expr.visit(self)?;
+        }
+
+        if let Some(expr) = &mut *node.r {
+            expr.visit(self)?;
+        }
+
+        Ok(())
+    }
+
+    /// Visit a function return node
+    fn visit_return(&mut self, node: &mut ReturnNode) -> Result<()>
+    where
+        Self: Sized,
+    {
+        if let Some(expression) = &mut node.value {
+            expression.visit(self)?;
+        }
+
+        Ok(())
+    }
+
+    /// Visit a string (literal) node
+    fn visit_string(&mut self, _node: &mut StringNode) -> Result<()> {
+        Ok(())
+    }
+
     /// Visit a ternary expression
     fn visit_ternary(&mut self, node: &mut TernaryNode) -> Result<()>
     where
@@ -285,6 +244,47 @@ pub trait TreeWalker {
         let _ = node.condition.visit(self);
         let _ = node.body.visit(self);
         let _ = node.else_clause.visit(self);
+
+        Ok(())
+    }
+
+    /// Visit a binary operation node
+    fn visit_unary_op(&mut self, node: &mut UnaryOpNode) -> Result<()>
+    where
+        Self: Sized,
+    {
+        node.expr.visit(self)?;
+
+        Ok(())
+    }
+
+    /// Visit a variable use node
+    fn visit_var(&mut self, _node: &mut VarNode) -> Result<()>
+    where
+        Self: Sized,
+    {
+        Ok(())
+    }
+
+    /// Visit a variable initialization node
+    fn visit_var_init(&mut self, node: &mut VarInitNode) -> Result<()>
+    where
+        Self: Sized,
+    {
+        if let Some(expr) = &mut node.value {
+            expr.visit(self)?;
+        }
+
+        Ok(())
+    }
+
+    /// Visit a `while` loop
+    fn visit_while(&mut self, node: &mut WhileNode) -> Result<()>
+    where
+        Self: Sized,
+    {
+        let _ = node.condition.visit(self);
+        let _ = node.body.visit(self);
 
         Ok(())
     }
