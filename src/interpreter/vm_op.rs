e0ad88af972ecd81f4643dcbe239ed431bcccaa2
mpd <mpd@jesters-court.net>
2023-04-01 23:19:02 -0700

    fix a missing doc link

--- src/interpreter/vm_op.rs
+++ src/interpreter/vm_op.rs
@@ -1,6 +1,6 @@
 /// Operations that can be communicated to the VM remotely.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum VmOp {
-    /// Run a CallOut function, identified by its index in the [`CallOuts`] heap
+    /// Run a CallOut function, identified by its index in the [`CallOuts`](crate::interpreter::call_outs::CallOuts) list
     RunCallOut(usize),
 }
